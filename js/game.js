2821191b0985cec75606c42d5026b06356d56338
Ghosts526 <Vault20boy@outlook.com>
2026-02-13 19:10:26 -0500

    Fixed the player controller with wasd

--- js/game.js
+++ js/game.js
@@ -73,7 +73,7 @@ var myGamePiece;
 function startGame()
 {
     myGameArea.start();
-    myGamePiece = new  component(30, 30, "red", 10, 10);
+    myGamePiece = new component(30, 30, "red", 10, 10);
 }
 
 var myGameArea = {
@@ -85,19 +85,16 @@ var myGameArea = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
         window.addEventListener('keydown', function(e) {
-            //myGameArea.keys = (myGameArea.keys || []);
-            //myGameArea.keys[e.keyCode] = true;
-            if (e.key === 'w') myGamePiece.speedY -= 1;
-            if (e.key === 's') myGamePiece.speedY += 1;
-            if (e.key === 'a') myGamePiece.speedX -= 1;
-            if (e.key === 'd') myGamePiece.speedX += 1;
+            if (e.key === 'w') myGamePiece.speedUp = -1;
+            if (e.key === 's') myGamePiece.speedDown = 1;
+            if (e.key === 'a') myGamePiece.speedLeft = -1
+            if (e.key === 'd') myGamePiece.speedRight = 1;
         })
         window.addEventListener('keyup', function(e) {
-            //myGameArea.keys[e.keyCode] = false;
-            if (e.key === 'w') myGamePiece.speedY += 1;
-            if (e.key === 's') myGamePiece.speedY -= 1;
-            if (e.key === 'a') myGamePiece.speedX += 1;
-            if (e.key === 'd') myGamePiece.speedX -= 1;
+            if (e.key === 'w') myGamePiece.speedUp = 0;
+            if (e.key === 's') myGamePiece.speedDown = 0;
+            if (e.key === 'a') myGamePiece.speedLeft = 0;
+            if (e.key === 'd') myGamePiece.speedRight = 0;
         })
     }, 
     clear : function()
@@ -110,8 +107,10 @@ function component(width, height, color, x, y)
 {
     this.width = width;
     this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
+    this.speedUp = 0;
+    this.speedDown = 0;
+    this.speedLeft = 0;
+    this.speedRight = 0;
     this.x = x;
     this.y = y;
     this.update = function() 
@@ -122,43 +121,14 @@ function component(width, height, color, x, y)
     }
     this.newPos = function()
     {
-        this.x += this.speedX;
-        this.y += this.speedY;
+        this.x += this.speedLeft + this.speedRight;
+        this.y += this.speedUp + this.speedDown;
     }
 }
 
 function updateGameArea()
 {
     myGameArea.clear();
-    /*
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;
-    if (myGameArea.keys && myGameArea.keys[65]) {myGamePiece.speedX = -1; }
-    if (myGameArea.keys && myGameArea.keys[68]) {myGamePiece.speedX = 1; }
-    if (myGameArea.keys && myGameArea.keys[87]) {myGamePiece.speedY = -1; }
-    if (myGameArea.keys && myGameArea.keys[83]) {myGamePiece.speedY = 1; } */
     myGamePiece.newPos();
     myGamePiece.update();
-}
-
-function moveup() {
-  myGamePiece.speedY -= 1;
-}
-
-function movedown() {
-  myGamePiece.speedY += 1;
-}
-
-function moveleft() {
-  myGamePiece.speedX -= 1;
-}
-
-function moveright() {
-  myGamePiece.speedX += 1;
-}
-
-function stopMove()
-{
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;
 }
