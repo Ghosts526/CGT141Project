b645714b01f9a87cd34d542a9d7bc29015be6950
Ghosts526 <ezavala526@outlook.com>
2026-02-23 21:54:10 -0500

    Changed Enemy Wave System

--- js/Enemy.js
+++ js/Enemy.js
@@ -21,7 +21,7 @@ export class Enemy
     }
 
     // Updates the enemy location based by its angle and speed
-    newPos(player)
+    newPos()
     {
         this.followPlayer(player);
         this.x += this.addX, this.y += this.addY;
--- js/WaveSystem.js
+++ js/WaveSystem.js
@@ -27,26 +27,9 @@ export class WaveSystem {
 
         if (this.enemiesSpawning > 0 && this.timer >= 100)
         {
-            let x = 0, y = 0, angle = 0;
-            let i = Math.floor(Math.random() * 4);
-
-            if (i == 0) {
-                x = Math.floor(Math.random() * gameArea.canvas.width);
-                y = 0;
-                angle = Math.PI;
-            } else if (i == 1) {
-                x = Math.floor(Math.random() * gameArea.canvas.width);
-                y = gameArea.canvas.height;
-                angle = 0;
-            } else if (i == 2) {
-                x = 0;
-                y = Math.floor(Math.random() * gameArea.canvas.height);
-                angle = Math.PI / 2;
-            } else {
-                x = gameArea.canvas.width;
-                y = Math.floor(Math.random() * gameArea.canvas.height);
-                angle = Math.PI / -2
-            }
+            let x = gameArea.canvas.width;
+            let y = Math.floor(Math.random() * gameArea.canvas.height);
+            let angle = -Math.PI / 2;
 
             enemies.push(new Enemy(x, y, 40, 40, angle, "images/Spaceship.png")); // Need to add the width, height, and angle
             this.enemiesSpawning--;
